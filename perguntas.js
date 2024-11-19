3910516c70ad4d65d02234211376508554ff9cc5
josé <romaozinho2006@gmail.com>
2024-11-19 07:29:23 -0300

    Primeiro commit

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Biologia',
+    'O que é mitose?',
+    'É o processo de divisão celular que resulta em duas células idênticas.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula da água?',
+    'H₂O.'
+)
+
+criaCartao(
+    'Física/História da Ciência',
+    'Quem foi Albert Einstein?',
+    'Um físico famoso pela teoria da relatividade.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'É o processo em que plantas produzem energia usando luz, água e CO₂.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor planeta do sistema solar?',
+    'Mercúrio.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que são verbos transitivos?',
+    'Verbos que precisam de complemento para fazer sentido.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Japão?',
+    'Tóquio.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que significa "pi" em matemática?',
+    'É uma constante que representa a razão entre a circunferência e o diâmetro de um círculo, aproximadamente 3,14.'
+)
